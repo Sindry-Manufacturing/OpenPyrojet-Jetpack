25ec760abecf06c35995a0fd7dc5fdf2e3146fa3
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2022-03-26 13:17:28 +0100

    implemented basic application modes (configuration vs normal functionality) when wifi is not configured, we enter configuration mode with (eventually) Wifi in AP mode

--- main/src/main.c
+++ main/src/main.c
@@ -13,18 +13,25 @@
 #include "fs_init.h"
 #include "websocket_server_start.h"
 
-void app_main(void) {
+static const char* TAG = "application";
 
+void app_main(void) {
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(fs_init());
+    ESP_ERROR_CHECK(jetpack_init()); // Config and IO
 
-    ESP_ERROR_CHECK(jetpack_init());
+    if (strlen(config.wifi.ssid) > 0) {
+        ESP_LOGI(TAG, "normal mode");
 
-    wifi_config(&(config.wifi));
+        wifi_config(&(config.wifi));
 
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    ESP_ERROR_CHECK(wifi_connect());
-
-    ESP_ERROR_CHECK(websocket_server_start());
+        ESP_ERROR_CHECK(esp_netif_init());
+        ESP_ERROR_CHECK(esp_event_loop_create_default());
+        ESP_ERROR_CHECK(wifi_connect());
+        ESP_ERROR_CHECK(websocket_server_start());
+    } else {
+        ESP_LOGI(TAG, "configuration mode");
+        ESP_LOGW(TAG, "Configuration mode is not yet implemented!");
+        ESP_LOGW(TAG, "Go to 'main/config/' and copy 'default.json' into 'user.json', then fill in the Wifi settings");
+    }
 }
