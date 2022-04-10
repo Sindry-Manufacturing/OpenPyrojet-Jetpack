48523d926d0786e464ed0310916fee1018c6b3fb
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2022-04-11 01:01:13 +0200

    don't initialise nozzle pins if the mask is empty

--- main/src/jetpack_io.c
+++ main/src/jetpack_io.c
@@ -19,14 +19,19 @@ static uint64 gpio_pin_bit_mask(const Config* config) {
 
 esp_err_t jetpack_io_init(const Config* config) {
     uint64 pinBitMask = gpio_pin_bit_mask(config);
-    gpio_config_t gpio = {
-        .intr_type = GPIO_INTR_DISABLE,
-        .mode = GPIO_MODE_OUTPUT,
-        .pin_bit_mask = pinBitMask,
-        .pull_down_en = 0, // disabled
-        .pull_up_en = 0 // disabled
-    };
-    return gpio_config(&gpio);
+    if (pinBitMask != 0) {
+        gpio_config_t gpio = {
+            .intr_type = GPIO_INTR_DISABLE,
+            .mode = GPIO_MODE_OUTPUT,
+            .pin_bit_mask = pinBitMask,
+            .pull_down_en = 0, // disabled
+            .pull_up_en = 0 // disabled
+        };
+        return gpio_config(&gpio);
+    } else {
+        ESP_LOGW(TAG, "no nozzle pins configured");
+        return ESP_OK;
+    }
 }
 
 void jetpack_io_fire(uint8 nozzleId) {
